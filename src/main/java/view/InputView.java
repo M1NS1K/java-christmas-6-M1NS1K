b94fd467c23beb3212fad9454f8853802c5880cb
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-13 04:32:32 +0900

    [view] readMenu 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 
 public class InputView {
 
@@ -17,4 +18,14 @@ public class InputView {
         return date;
     }
     // ...
+
+    public HashMap<String, Integer> readMenu() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+        String input = Console.readLine();
+
+        return null;
+    }
+
+
 }
