e7be8a1dd4c0bb6676b51841e64277dcceface0a
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-13 04:28:24 +0900

    [view] readDate 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,11 +3,18 @@ package view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+
         String input = Console.readLine();
-        // ...
-        return 0;
+        int date = Integer.parseInt(input);
+
+        if(date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+
+        return date;
     }
     // ...
 }
--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -1,4 +1,10 @@
 package view;
 
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class InputViewTest {
+
 }
