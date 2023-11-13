1f331be341a8a0d4d74897b0441f34bdb60d898d
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-13 18:00:05 +0900

    [test] 공백 확인 테스트 구현

--- src/main/java/model/Service.java
+++ src/main/java/model/Service.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.junit.platform.commons.util.StringUtils;
+import org.mockito.internal.util.StringUtil;
 
 public class Service {
     private Service() {}
@@ -13,11 +15,17 @@ public class Service {
         private final static Service INStANCE = new Service();
     }
 
-    public void makeMenuOrderStringToList(String menuOrder) {
+    public void containsWhitespaceException(String menuOrder) {
+        if(StringUtils.containsWhitespace(menuOrder)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public List<String> makeMenuOrderStringToList(String menuOrder) {
         List<String> menuOrderList = new ArrayList<>(List.of(menuOrder.split(",")));
 
         //예외처리
-
+        return menuOrderList;
     }
 
     public void makeMenuOrderListToMap(List<String> menuOrderList) {
--- src/test/java/model/ServiceTest.java
+++ src/test/java/model/ServiceTest.java
@@ -1,8 +1,23 @@
 package model;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ServiceTest {
-
-    @DisplayName("")
+    @DisplayName("공백 확인 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "타파스-1, 제로콜라-1",
+            "타 파스-1, 제로콜라-2",
+            "타파스 -1, 제로콜라-3"
+    })
+    void testContainsWhitespace(String menuOrder) {
+        assertThatThrownBy(() -> {
+            Service.getInstance().containsWhitespaceException(menuOrder);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
