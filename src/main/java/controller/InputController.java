59e299f9de75c82fae181a4d055dd4a8bc288e21
M1NS1K <rlaalstlr2001@gmail.com>
2023-11-14 22:33:32 +0900

    [model] Domain setDate 구현

--- src/main/java/controller/InputController.java
+++ src/main/java/controller/InputController.java
@@ -1,11 +1,18 @@
 package controller;
 
+import model.Domain;
 import model.Service;
 import view.InputView;
 
 public class InputController {
-    private final Service SERVICE = Service.getInstance();
+    private final Domain DOMAIN = Domain.getInstance();
     private final InputView INPUT = new InputView();
 
+    public void setMenuOrder() {
+        DOMAIN.setMenuOrder(INPUT.readMenu());
+    }
 
+    public void setDate() {
+        DOMAIN.setDate(INPUT.readDate());
+    }
 }
--- src/main/java/model/Domain.java
+++ src/main/java/model/Domain.java
@@ -5,10 +5,24 @@ import java.util.Map;
 public class Domain {
     private final Service SERVICE = Service.getInstance();
     private Map<String, Integer> menuOrder;
+    private int date;
 
     public void setMenuOrder(String menuOrder) {
         this.menuOrder = SERVICE.makeMenuOrderListToMap(
                 SERVICE.makeMenuOrderStringToList(menuOrder));
     }
 
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    private Domain() {}
+
+    private class SINGLETON {
+        public static final Domain INSTANCE = new Domain();
+    }
+
+    public static Domain getInstance() {
+        return SINGLETON.INSTANCE;
+    }
 }
